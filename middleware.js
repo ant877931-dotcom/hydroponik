50ee72633d58504f986c3f9fdde3682d350d671c
ant877931-dotcom <ant877931@gmail.com>
2026-08-14 09:51:17 +0700

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -1,11 +1,16 @@
+import { NextResponse } from 'next/server';
+
 export function middleware(request) {
   const secret = request.headers.get('x-sentinel-secret');
 
+  // Jika secret key tidak cocok atau tidak ada, tendang ke halaman 403 atau blokir
   if (secret !== 'S3CR3T_K3Y_H1DR0P0N1K_2026') {
-    return new Response('403 Forbidden: Direct access denied.', {
+    return new NextResponse('403 Forbidden: Direct access denied.', {
       status: 403,
     });
   }
+
+  return NextResponse.next();
 }
 
 export const config = {

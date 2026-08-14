05df352395fe19901727fcaac59f31b5b1ab8923
ant877931-dotcom <ant877931@gmail.com>
2026-08-14 09:57:41 +0700

    Update vercel.json dan kembalikan middleware.js

--- middleware.js
+++
@@ -1,7 +0,0 @@
-export function middleware(request) {
-  const secret = request.headers.get('x-sentinel-secret');
-
-  if (secret !== 'S3CR3T_K3Y_H1DR0P0N1K_2026') {
-    return new Response('403 Forbidden: Direct access denied.', { status: 403 });
-  }
-}
